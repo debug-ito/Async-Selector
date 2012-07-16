995b83929b17b7a51e84216b9a492a46fd9ae661
Toshio Ito <debug.ito@gmail.com>
2012-07-16 22:10:35 +0900

    Add SYNOPSIS.

--- lib/Async/Selector.pm
+++ lib/Async/Selector.pm
@@ -27,7 +27,37 @@ our $VERSION = "0.01";
 
 =head1 SYNOPSIS
 
-
+    use Async::Selector;
+    
+    my $selector = Async::Selector->new();
+    
+    ## Register resource
+    my $resource = "some text.";  ## 10 bytes
+    $selector->register(
+        resource_A => sub {
+            my $threshold = shift;
+            return length($resource) >= $threshold ? $resource : undef;
+        }
+    );
+    
+    
+    ## Select the resource with a callback.
+    $selector->select(
+        sub {
+            my ($id, %resource) = @_;
+            print "$resource{resource_A}\n";
+            return 1;
+        },
+        resource_A => 20,  ## Tell me when the resource gets more than 20 bytes!
+    );
+    
+    
+    ## Append data to the resource
+    $resource .= "data";  ## 14 bytes
+    $selector->trigger('resource_A'); ## Nothing happens
+    
+    $resource .= "more data";  ## 23 bytes
+    $selector->trigger('resource_A'); ## The callback prings 'some text.datamore data'
 
 
 =head1 DESCRIPTION
