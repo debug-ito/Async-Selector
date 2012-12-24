0724c5933ce5ac3f4207ae75c3f3aba1b5657c91
Toshio Ito <debug.ito@gmail.com>
2012-12-24 10:55:34 +0900

    Watcher.pm: add SYNOPSIS to POD.

--- lib/Async/Selector/Watcher.pm
+++ lib/Async/Selector/Watcher.pm
@@ -69,7 +69,31 @@ Async::Selector::Watcher - Representation of resource watch in Async::Selector
 
 =head1 SYNOPSIS
 
-B<TODO: Write SYNOPSIS, check its correctness and paste it here.>
+
+    use Async::Selector;
+    
+    my $s = Async::Selector->new();
+    
+    setup_resources_with($s);
+    
+    ## Obtain a watcher from Selector.
+    my $watcher = $s->watch(a => 1, b => 2, sub {
+        my ($w, %res) = @_;
+        handle_a($res{a}) if exists $res{a};
+        handle_b($res{b}) if exists $res{b};
+    });
+    
+    ## Is the watcher active?
+    $watcher->active;                          ## => true
+    
+    ## Get the list of watched resources
+    my @resources = sort $watcher->resources;  ## => ('a', 'b')
+    
+    ## Get the watcher conditions
+    my %conditions = $watcher->conditions;     ## => (a => 1, b => 2)
+    
+    ## Cancel the watcher
+    $watcher->cancel;
 
 
 =head1 DESCRIPTION
@@ -89,15 +113,6 @@ L<Async::Selector::Watcher> objects are created by C<watch()>, C<watch_lt()> and
 
 In the following description, C<$watcher> is an L<Async::Selector::Watcher> object.
 
-=head2 $watcher->cancel();
-
-Cancel the watch.
-
-The C<$watcher> then becomes inactive and is removed from the L<Async::Selector> object it used to belong to.
-
-You should call C<cancel()> method on every watchers at some point.
-Otherwise, watchers would persist in an L<Async::Selector> obejct, causing memory leak.
-
 =head2 $is_active = $watcher->active();
 
 Returns true if the L<Async::Selector::Watcher> is active. Returns false otherwise.
@@ -108,8 +123,17 @@ Callback functions of active watchers can be executed if the watched resources g
 Inactive watchers are the ones that have been removed from L<Async::Selector> objects.
 Their callback functions are never executed any more.
 
+Note that watchers are automatically canceled and become inactive when their parent L<Async::Selector> object is destroyed.
 
-=head2 @resources = @watcher->resources();
+
+=head2 $watcher->cancel();
+
+Cancel the watch.
+
+The C<$watcher> then becomes inactive and is removed from the L<Async::Selector> object it used to belong to.
+
+
+=head2 @resources = $watcher->resources();
 
 Returns the list of resource names that are watched by this L<Async::Selector::Watcher> object.
 
@@ -120,7 +144,6 @@ Returns the list of resource names that are watched by this L<Async::Selector::W
 Returns a hash whose keys are the resource names that are watched by this L<Async::Selector::Watcher> object,
 and values are the condition inputs for the resources.
 
-C<%conditions> are the same as C<< $name => $condition_input >> pairs that were given to C<watch()> method of L<Async::Selector>.
 
 
 =head1 SEE ALSO
